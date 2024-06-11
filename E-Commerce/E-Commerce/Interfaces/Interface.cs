b4e19081e15a4a0fedecda6b9e7e8d0ce4da2ef4
Elmer Velazquez <elmerVelazquez11@gmail.com>
2024-06-11 11:34:59 -0400

    estructura inicial del proyecto2

---
+++ E-Commerce/E-Commerce/Interfaces/Interface.cs
@@ -0,0 +1,6 @@
+﻿namespace E_Commerce.Interfaces
+{
+    public interface Interface
+    {
+    }
+}
--- E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843160a8d7146666da2a236ee6ae491356f6cd5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42486d6a2ce0f26f57afdcd22adc1887a1198563")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
