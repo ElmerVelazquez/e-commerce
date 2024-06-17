0c3234bc130e9472bd985a16523e2c0135f95eca
jaeljjh <jaeljimehera@gmail.com>
2024-06-17 09:55:22 -0400

    Login y registro completados

--- E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12515cfa9ae7e0b4d79460b6acd4f0acccffe029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a1ee2d403ffd821aa9ff1c495f701b5a2ebd2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
