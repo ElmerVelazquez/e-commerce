0c1b17647e8c45620baf35bb2befb59eaec32d1d
Elmer Velazquez <elmerVelazquez11@gmail.com>
2024-07-10 15:21:55 -0400

    refreshtoken

--- E-Commerce/E-Commerce/Models/Password.cs
+++ E-Commerce/E-Commerce/Models/Password.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public string PasswordHash { get; set; }
+        public string RefreshToken { get; set; }
         public virtual User User { get; set; }
     }
 }
