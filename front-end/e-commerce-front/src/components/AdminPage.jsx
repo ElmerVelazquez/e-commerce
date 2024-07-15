eef5b52864e5dd2000af32b2f7b73015431693bb
jaeljjh <jaeljimehera@gmail.com>
2024-07-15 15:00:37 -0400

    cambios en el adminPage

--- front-end/e-commerce-front/src/components/AdminPage.jsx
+++ front-end/e-commerce-front/src/components/AdminPage.jsx
@@ -6,7 +6,6 @@ import withReactContent from 'sweetalert2-react-content';
 
 const MySwal = withReactContent(Swal);
 
-// Mapeo de categoryId a nombres de categorías
 const categories = {
     1: 'Laptops',
     2: 'Teléfonos',
@@ -15,14 +14,14 @@ const categories = {
 };
 
 function AdminPage() {
-    const { user } = useAuth(); // Obtener el usuario autenticado del contexto
-    const navigate = useNavigate(); // Hook para la navegación
-    const [products, setProducts] = useState([]); // Estado para los productos
-    const [newProduct, setNewProduct] = useState({ name: '', price: 0, description: '', stock: 0, urlImg: '', categoryId: '' }); // Estado para el nuevo producto
-    const [currentPage, setCurrentPage] = useState(1); // Estado para la página actual de paginación
-    const productsPerPage = 9; // Número de productos por página
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const [products, setProducts] = useState([]);
+    const [newProduct, setNewProduct] = useState({ name: '', price: 0, description: '', stock: 0, urlImg: '', categoryId: '' });
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 9;
+    const [selectedCategory, setSelectedCategory] = useState('');
 
-    // useEffect para redirigir si el usuario no es administrador
     useEffect(() => {
         if (!user || user.rol !== 'admin') {
             MySwal.fire({
@@ -31,30 +30,26 @@ function AdminPage() {
                 icon: 'error',
                 confirmButtonText: 'OK'
             }).then(() => {
-                navigate('/login'); // Redirigir a la página de login
+                navigate('/login');
             });
         }
     }, [user, navigate]);
 
-    // useEffect para obtener los productos desde la API
     useEffect(() => {
         const fetchProducts = async () => {
             try {
                 const response = await fetch(import.meta.env.VITE_API_PRODUCT_URL);
                 const data = await response.json();
-                // Ordenar los productos por fecha de creación (más recientes primero)
                 setProducts(data.value.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)));
             } catch (error) {
-                console.error('Error fetching products:', error); // Manejo de errores
+                console.error('Error fetching products:', error);
             }
         };
         fetchProducts();
     }, []);
 
-    // Manejo de la adición de nuevos productos
     const handleAddProduct = async (e) => {
         e.preventDefault();
-    
         try {
             const response = await fetch(import.meta.env.VITE_API_PRODUCT_URL, {
                 method: 'POST',
@@ -62,7 +57,7 @@ function AdminPage() {
                     'Content-Type': 'application/json',             
                     'Authorization': 'bearer ' + user.token,
                 },
-                body: JSON.stringify(newProduct), // Enviar el nuevo producto en el cuerpo de la solicitud
+                body: JSON.stringify(newProduct),
             });
             
             if (!response.ok) {
@@ -71,10 +66,10 @@ function AdminPage() {
             }
     
             const responseData = await response.json();
-            const addedProduct = responseData.value; // Asumiendo que solo se agrega un producto a la vez
+            const addedProduct = responseData.value;
     
-            setProducts([addedProduct, ...products]); // Actualizar el estado con el nuevo producto
-            setNewProduct({ name: '', description: '', price: 0, stock: 0, urlImg: '', categoryId: '' }); // Limpiar el formulario
+            setProducts([addedProduct, ...products]);
+            setNewProduct({ name: '', description: '', price: 0, stock: 0, urlImg: '', categoryId: '' });
             
             MySwal.fire({
                 title: 'Éxito',
@@ -93,7 +88,6 @@ function AdminPage() {
         }
     };
     
-    // Manejo de la eliminación de productos
     const handleDeleteProduct = async (productId) => {
         try {
             const response = await fetch(`${import.meta.env.VITE_API_PRODUCT_URL}/${productId}`, {
@@ -109,7 +103,7 @@ function AdminPage() {
                 throw new Error(errorData.message || 'Error al eliminar producto');
             }
 
-            setProducts(products.filter(product => product.id !== productId)); // Actualizar el estado filtrando el producto eliminado
+            setProducts(products.filter(product => product.id !== productId));
 
             MySwal.fire({
                 title: 'Éxito',
@@ -128,122 +122,166 @@ function AdminPage() {
         }
     };
 
-    // Paginación: determinar el índice de los productos a mostrar en la página actual
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+    const filteredProducts = selectedCategory
+        ? products.filter(product => product.categoryId === Number(selectedCategory))
+        : products;
+    const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
 
-    // Función para cambiar de página
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
-        <div className="container mx-auto p-8">
-            <h1 className="text-2xl font-bold mb-4">Página del administrador</h1>
-            <form onSubmit={handleAddProduct} className="mb-4">
-                <h2 className="text-xl font-bold mb-2">Agregar nuevos productos</h2>
-                {/* Sección del nombre de producto */}
-                <div className="mb-2">
-                    <label className="block text-gray-700">Nombre del producto</label>
-                    <input
-                        type="text"
-                        value={newProduct.name}
-                        onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-                        className="w-full px-3 py-2 border rounded"
-                        required
-                    />
-                </div>
-                {/* Sección de descripción */}
-                <div className="mb-2">
-                    <label className="block text-gray-700">Descripción</label>
-                    <textarea
-                        value={newProduct.description}
-                        onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
-                        className="w-full px-3 py-2 border rounded"
-                        required
-                    />
-                </div>
-                {/* Sección de precio */}
-                <div className="mb-2">
-                    <label className="block text-gray-700">Precio</label>
-                    <input
-                        type="number"
-                        value={newProduct.price}
-                        onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
-                        className="w-full px-3 py-2 border rounded"
-                        required
-                    />
-                </div>
-                {/* Sección de stock */}
-                <div className="mb-2">
-                    <label className="block text-gray-700">Stock</label>
-                    <input
-                        type="number"
-                        value={newProduct.stock}
-                        onChange={(e) => setNewProduct({ ...newProduct, stock: e.target.value })}
-                        className="w-full px-3 py-2 border rounded"
-                        required
-                    />
-                </div>
-                {/* Sección de URL de imagen */}
-                <div className="mb-2">
-                    <label className="block text-gray-700">Url img</label>
-                    <input
-                        type="text"
-                        value={newProduct.urlImg}
-                        onChange={(e) => setNewProduct({ ...newProduct, urlImg: e.target.value })}
-                        className="w-full px-3 py-2 border rounded"
-                        required
-                    />
-                </div>
-                {/* Sección de selección de categoría */}
-                <div className="mb-2">
-                    <label className="block text-gray-700">Categoría</label>
-                    <select
-                        value={newProduct.categoryId}
-                        onChange={(e) => setNewProduct({ ...newProduct, categoryId: e.target.value })} 
-                        className="w-full px-3 py-2 border rounded"
-                        required
-                    >
-                        <option value="">Selecciona una categoría</option>
-                        <option value="1">Laptops</option>
-                        <option value="2">Teléfonos</option>
-                        <option value="3">Desktops</option>
-                        <option value="4">Accesorios</option>
-                    </select>
-                </div>
-                <button type="submit" className="bg-red-500 text-white px-4 py-2 rounded">
-                    Agregar productos
-                </button>
-            </form>
-            <h2 className="text-xl font-bold mb-2">Productos</h2>
-            {/* Grid de productos */}
-            <div className="grid grid-cols-3 gap-4">
-                {currentProducts.map(product => (
-                    <div key={product.id} className="border p-4 rounded">
-                        <h3 className="text-lg font-bold">{product.name}</h3>
-                        <p className="text-gray-700">{product.description}</p>
-                        <p className="text-gray-700">${product.price}</p>
-                        <p className="text-gray-700">Categoría: {categories[product.categoryId]}</p>
+        <div className="flex">
+            <div className="w-1/5 bg-gray-200 p-4">
+                <h2 className="text-lg font-bold mb-2">Categorías</h2>
+                <ul>
+                    {Object.entries(categories).map(([id, name]) => (
+                        <li key={id} className="mb-2">
+                            <button
+                                onClick={() => {
+                                    setSelectedCategory(id);
+                                    setCurrentPage(1);
+                                }}
+                                className={`w-full text-left px-2 py-1 ${selectedCategory === id ? 'bg-red-500 text-white' : 'hover:bg-gray-300'}`}
+                            >
+                                {name}
+                            </button>
+                        </li>
+                    ))}
+                    <li>
                         <button
-                            onClick={() => handleDeleteProduct(product.id)}
-                            className="bg-red-500 text-white px-4 py-2 rounded mt-2"
+                            onClick={() => {
+                                setSelectedCategory('');
+                                setCurrentPage(1);
+                            }}
+                            className={`w-full text-left px-2 py-1 ${selectedCategory === '' ? 'bg-red-500 text-white' : 'hover:bg-gray-300'}`}
                         >
-                            Eliminar
+                            Todos
                         </button>
-                    </div>
-                ))}
+                    </li>
+                </ul>
             </div>
-            {/* Paginación */}
-            <div className="flex justify-center mt-4">
-                {Array.from({ length: Math.ceil(products.length / productsPerPage) }, (_, index) => (
+            <div className="w-4/5 p-8">
+                <div className="flex justify-between items-center mb-4">
+                    <h1 className="text-2xl font-bold">Página del administrador</h1>
                     <button
-                        key={index + 1}
-                        onClick={() => paginate(index + 1)}
-                        className={`px-4 py-2 mx-1 ${currentPage === index + 1 ? 'bg-red-500 text-white' : 'bg-gray-300'}`}
+                        onClick={() => navigate('/')}
+                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
                     >
-                        {index + 1}
+                        Ir a productos
                     </button>
-                ))}
+                </div>
+                <form onSubmit={handleAddProduct} className="mb-4">
+                    <h2 className="text-xl font-bold mb-2">Agregar nuevos productos</h2>
+                    <div className="mb-2">
+                        <label className="block text-gray-700">Nombre del producto</label>
+                        <input
+                            type="text"
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="block text-gray-700">Descripción</label>
+                        <textarea
+                            value={newProduct.description}
+                            onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="block text-gray-700">Precio</label>
+                        <input
+                            type="number"
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="block text-gray-700">Stock</label>
+                        <input
+                            type="number"
+                            value={newProduct.stock}
+                            onChange={(e) => setNewProduct({ ...newProduct, stock: e.target.value })}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="block text-gray-700">URL de la imagen</label>
+                        <input
+                            type="text"
+                            value={newProduct.urlImg}
+                            onChange={(e) => setNewProduct({ ...newProduct, urlImg: e.target.value })}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label className="block text-gray-700">Categoría</label>
+                        <select
+                            value={newProduct.categoryId}
+                            onChange={(e) => setNewProduct({ ...newProduct, categoryId: e.target.value })}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        >
+                            <option value="">Seleccione una categoría</option>
+                            {Object.entries(categories).map(([id, name]) => (
+                                <option key={id} value={id}>{name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
+                    >
+                        Agregar Producto
+                    </button>
+                </form>
+                <div>
+                    <h2 className="text-xl font-bold mb-2">Productos</h2>
+                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                        {currentProducts.map((product) => (
+                            <li key={product.id} className="border p-4 rounded flex flex-col">
+                                <h3 className="text-lg font-bold mb-2">{product.name}</h3>
+                                <img src={product.urlImg} alt={product.name} className="w-full h-48 object-contain mb-2" />
+                                <p className="flex-grow mb-2">{product.description}</p>
+                                <div className="flex justify-between items-center mb-2">
+                                    <span><strong>Precio:</strong> ${product.price}</span>
+                                    <span><strong>Stock:</strong> {product.stock}</span>
+                                </div>
+                                <button
+                                    onClick={() => handleDeleteProduct(product.id)}
+                                    className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 mt-auto"
+                                >
+                                    Eliminar
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="flex justify-center mt-4">
+                        <nav>
+                            <ul className="inline-flex items-center">
+                                {Array.from({ length: Math.ceil(filteredProducts.length / productsPerPage) }, (_, i) => (
+                                    <li key={i}>
+                                        <button
+                                            onClick={() => paginate(i + 1)}
+                                            className={`px-3 py-1 border ${currentPage === i + 1 ? 'bg-red-500 text-white' : 'bg-white text-red-500 hover:bg-red-300'}`}
+                                        >
+                                            {i + 1}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
             </div>
         </div>
     );
