ffa47d0c78d6c0cd50e8e2a41d8c85662c1c30c7
Elmer Velazquez <elmerVelazquez11@gmail.com>
2024-07-22 11:38:47 -0400

    urlemailverification

--- E-Commerce/E-Commerce/Controllers/EmailVerificationController.cs
+++ E-Commerce/E-Commerce/Controllers/EmailVerificationController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using E_Commerce.Interfaces;
 using E_Commerce.Utilities;
 using Microsoft.AspNetCore.Cors;
+using System.Net.Mail;
 
 namespace E_Commerce.Controllers
 {
@@ -21,47 +22,13 @@ namespace E_Commerce.Controllers
         }
         [AllowAnonymous]
         [HttpPost("sendcode")]
-        public async Task<IActionResult> Send([FromBody]string email)
+        public async Task<IActionResult> Send([FromBody] string email)
         {
             string codigo = _repo.SaveVerificationCode(email).Result.Value;
             if (codigo == null) return BadRequest(Result.Fail("Email no encontrado"));
             const string subject = "Verificación correo";
-            var body = @"
-                    <html>
-                    <head>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f2f2f2;
-                            }
-                            .container {
-                                max-width: 600px;
-                                margin: 0 auto;
-                                padding: 20px;
-                                background-color: #fff;
-                                border: 1px solid #ccc;
-                                border-radius: 5px;
-                                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                            }
-                            h1 {
-                                color: #333;
-                                text-align: center;
-                            }
-                            p {
-                                color: #666;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div class='container'>
-                            <h1>LincolnTech verificación de registro</h1>
-                            <p>Codigo de verificación:</p>
-                            <p>"+codigo+@"</p>
-                            <p>Si no has sido tú ignora este correo</p>
-                        </div>
-                    </body>
-                    </html>
-                ";
+            var body = _repo.generatebody("LincolnTech verificación de registro", "Codigo de verificación",codigo);
+           
             try
             {
                 await _repo.SendEmailAsync(email, subject, body);
@@ -69,28 +36,28 @@ namespace E_Commerce.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest(Result.Fail("Error al enviar el correo electronico: " + ex.Message)); 
-                    
-            }          
-            
+                return BadRequest(Result.Fail("Error al enviar el correo electronico: " + ex.Message));
+
+            }
+
         }
         [AllowAnonymous]
-        [HttpPost("checkcode/{code}")]
-        public async Task<IActionResult> Compare([FromBody] string email,string code)
+        [HttpGet("checkcode")]
+        public async Task<IActionResult> Compare([FromQuery] string email, [FromQuery] string code)
         {
-            var respons = await _repo.CompareVerificationCode(email,code);
+            var respons = await _repo.CompareVerificationCode(email, code);
             if (respons.IsSuccess)
             {
                 return Ok(respons);
-               
+
             }
             return BadRequest(respons);
 
         }
 
         [AllowAnonymous]
-         [HttpPost("passwordrecovery/{email}&{code}")]
-        public async Task<IActionResult> PasswordRecovery(string email, string code, [FromBody] string newPassword)
+        [HttpPost("passwordrecovery")]
+        public async Task<IActionResult> PasswordRecovery([FromQuery] string email, [FromQuery] string code, [FromBody] string newPassword)
         {
             var respons = await _repo.PasswordRecovery(email, code, newPassword);
             if (respons.IsSuccess)
@@ -101,8 +68,33 @@ namespace E_Commerce.Controllers
             return BadRequest(respons);
 
         }
+        [AllowAnonymous]
+        [HttpPost("SendVerificationUrl")]
+        public async Task<IActionResult> SendVerificationUrlAsync(string email, string UrlPageVerification)
+        {
+            string code = _repo.SaveVerificationCode(email).Result.Value;
+            if (code == null) return BadRequest(Result.Fail("Email no encontrado"));
 
+            //var verificationLink = Url.Action("Compare", "EmailVerification", new { email, code}, Request.Scheme);
+            var verificationLink = UrlPageVerification + $"?email={email}&code={code}";
+            var message = new MailMessage();
+            var subject = "Account Verification";
+            var body = _repo.generatebody("LincolnTech verificación de registro", "Codigo de verificación",
+                $"Por favor verifica tu cuenta haciendo click en este link: <a href=\"{verificationLink}\">Verify Account</a>");
+
+            try
+            {
+                await _repo.SendEmailAsync(email, subject, body);
+                return Ok(Result.Success());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(Result.Fail("Error al enviar el correo electronico: " + ex.Message));
+
+            }
+
+        }
     }
 
-
 }
+     
